53651df6615b56771ecddcf091503be73786bdd5
Dmitry Dergunov <dmitrynuc@gmail.com>
2020-10-21 19:15:46 -0400

    Updates

---
+++ precache-manifest.999f05e6519fbaad0d8fba24e2f49a4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871ea1981b65593ee0f4501248d62a02",
+    "url": "/react-catofriends/index.html"
+  },
+  {
+    "revision": "7db58a68d56cd5604cc5",
+    "url": "/react-catofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fc7555e7b26090810f55",
+    "url": "/react-catofriends/static/css/main.6da34a49.chunk.css"
+  },
+  {
+    "revision": "7db58a68d56cd5604cc5",
+    "url": "/react-catofriends/static/js/2.19331f0f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-catofriends/static/js/2.19331f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7555e7b26090810f55",
+    "url": "/react-catofriends/static/js/main.4e3f8a08.chunk.js"
+  },
+  {
+    "revision": "4ad1632c3f30f8fd8828",
+    "url": "/react-catofriends/static/js/runtime-main.d02b3894.js"
+  },
+  {
+    "revision": "b990a7119a7d879e2ca1b28cbad71758",
+    "url": "/react-catofriends/static/media/Retro Computer.b990a711.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-catofriends/precache-manifest.999f05e6519fbaad0d8fba24e2f49a4f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-catofriends/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
